f361494654f82ed2e16e4673664b7c4890708edc
vagrant <tk95@live.ca>
2019-07-26 19:34:34 +0000

    Changed the routes folder to contain pins, maps, users.js

---
+++ routes/maps.js
@@ -0,0 +1,27 @@
+/*
+ * All routes for Maps are defined here
+ * Since this file is loaded in server.js into api/maps,
+ *   these routes are mounted onto /maps
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    let query = `SELECT * FROM maps`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const maps = data.rows;
+        res.json({ maps });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
