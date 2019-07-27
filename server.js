31afa6ccd6b5a71624d46b439aa745caf1e62de2
Jessie Wang <wangj.5415@gmail.com>
2019-07-27 17:04:37 +0000

    made ajax changes to the PUT and DELETE routes

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ app.use(express.static("public"));
 
 // // Separated Routes for each Resource
 // // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
+// const usersRoutes = require("./routes/users");
 const mapsRoutes = require("./routes/maps");
 const pinsRoutes = require("./routes/pins");
 
 // // Mount all resource routes
 // // Note: Feel free to replace the example routes below with your own
-app.use("/users", usersRoutes(db));
+// app.use("/users", usersRoutes(db));
 app.use("/maps", mapsRoutes(db));
 app.use("/pins", pinsRoutes(db));
 
