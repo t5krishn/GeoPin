af552aeda314a764ae5577a26ca7f4ff327112bc
vagrant <tk95@live.ca>
2019-07-28 16:47:02 +0000

    Added comments to schema files and added my test file to .gitignore, added updateMap.js which is not complete

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -3,6 +3,8 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
+  -- username instead of name
   name TEXT NOT NULL,
   password TEXT NOT NULL
+  -- deleted flag instead of dropping user from db
 );
