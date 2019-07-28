e8c86517d3d0a0fba4f0f10fac7865e291687c83
vagrant <tk95@live.ca>
2019-07-28 14:01:53 +0000

    Added addUser.js query

--- db/queries/addUser.js
+++ db/queries/addUser.js
@@ -1,5 +1,19 @@
-const addUser = function () {
+const addUser = function (db, userParams) {
+    const query = `
+        INSERT INTO users (name, password)
+        VALUES ($1, $2)
+        RETURNING *;
+    `;
 
+    return db.query(query, userParams)
+        .then(res => {
+            if (res.rows) {
+                return res.rows[0];
+            } else {
+                return null;
+            }
+        })
+        .catch(err => { console.log(e) });
 }
 
 module.exports = addUser;
