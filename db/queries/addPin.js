fd5d088bce4877bf0742a77122175a5e6a5118f5
vagrant <tk95@live.ca>
2019-07-29 18:28:03 +0000

    Small edits in query files to fix minor bugs found while testing

--- db/queries/addPin.js
+++ db/queries/addPin.js
@@ -1,4 +1,9 @@
 // pinParams should be an array
+//  --> label, description, longitude, latitude, pin_thumbnail_url, map_id ONLY
+//  --> pinParams will need to make the empty fields null in order for sql query to work
+const incrementPinCountWithMapId = require('./incrementPinCountWithMapId');
+
+
 const addPin = function (pool, pinParams) {
     // update pin_count in map table as well
     const query = `
@@ -10,10 +15,12 @@ const addPin = function (pool, pinParams) {
     return pool.query(query, pinParams)
         .then(res => {
             if (res.rows) {
-                return res.rows[0];
+                incrementPinCountWithMapId(pool, pinParams[pinParams.length -1])
+                .catch(err2 => { console.log(err2) });
             } else {
                 return null;
             }
+            return res.rows[0];
         })
         .catch(err => { console.log(err) });
 }
