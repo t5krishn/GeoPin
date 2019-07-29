6bd3c9768b988165389ef2178302a7fa70c66daf
Kendall Rowe <kendall.rowe312@gmail.com>
2019-07-29 16:12:19 +0000

    Removed console logs

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,9 +1,7 @@
 // Extract a parameter from given url as a string
 const getMapIDFromURL = (url) => {
   const urlArray = url.split("/");
-  console.log("array: ", urlArray);
   const mapsIndex = urlArray.indexOf("maps");
-  console.log("index: ", mapsIndex);
   if (mapsIndex !== -1) {
     return urlArray[mapsIndex + 1];
   }
