30f5dfe0921972a1412354aaf4b984416a282044
vagrant <tk95@live.ca>
2019-07-29 13:35:09 +0000

    Added limit for getAllMaps.js and deleted flag in query

--- db/queries/getAllMaps.js
+++ db/queries/getAllMaps.js
@@ -1,10 +1,12 @@
-const getAllMaps = function (pool) {
+const getAllMaps = function (pool, limit = 10) {
 
     const query = `
         SELECT *
         FROM maps
+        WHERE maps.deleted = FALSE
+        LIMIT $1;
     `;
-    return pool.query(query, [])
+    return pool.query(query, [limit])
         .then(res => {
             if (res.rows) {
                 return res.rows;
