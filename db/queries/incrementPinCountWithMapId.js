85053b5cb86d31d9edc2fb3daba976181d88fb52
vagrant <tk95@live.ca>
2019-07-29 13:29:27 +0000

    Started implementing the delete queries and incorporating the pin counts into the delete queries

---
+++ db/queries/incrementPinCountWithMapId.js
@@ -0,0 +1,22 @@
+const incrementPinCountWithMapId = function (pool, map_id) {
+
+    const query = `
+        UPDATE maps
+        SET pin_count = (pin_count + 1)
+        WHERE maps.id = $1
+        RETURNING *;
+    `;
+
+    return pool.query(query, [map_id])
+    .then(res => {
+        if (res.rows) {
+            return res.rows[0];
+        } else {
+            return null;
+        }
+    })
+    .catch(err => { console.log(err) });
+
+}
+
+module.exports = incrementPinCountWithMapId;
