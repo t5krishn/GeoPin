58c82dbae0c07b56646b2e176a6420ebc3200c16
Kendall Rowe <kendall.rowe312@gmail.com>
2019-07-31 16:40:07 +0000

    Enabled recentering onto infowindows when opened and ensuring coordinates are correctly binding to markers

--- public/scripts/maps_edit.js
+++ public/scripts/maps_edit.js
@@ -8,27 +8,17 @@ $(() => {
     const url = event.target.getAttribute("action");
     ajaxGetSinglePin(url)
     .done((pin) => {
+
+      closeInfoWindowIfPresent();
+
       pin.geometry = { location: new google.maps.LatLng(pin.latitude, pin.longitude) };
       removeAllMarkers();
-      createMarker(pin)
+      createMarker(pin, pin);
 
-      if (infowindow) {
-        $(document).on("submit", "#pin-create-form", submitPinForm).off();
 
-        infowindow.setContent("");
-        infowindow.close();
-      }
+      map.setCenter(pin.geometry.location);
 
-      const editParams = {
-        label: pin.label,
-        description: pin.description,
-        pin_thumbnail_url: pin.pin_thumbnail_url,
-        mapID: pin.map_id,
-        url: `/maps/${pin.map_id}/pins/${pin.id}/edit/?_method=PUT`
-      };
 
-      infowindow = genInfoWindow(pin, editParams);
-      infowindow.open(map, allMarkers[0]);
     });
   });
 
@@ -40,6 +30,7 @@ $(() => {
     .done(() => {
       const formRow = event.target.closest(".form-row");
       event.target.closest("#all-pins").removeChild(formRow)
+      closeInfoWindowIfPresent();
     });
   });
 
