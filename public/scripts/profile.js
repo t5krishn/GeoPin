edf1fd6a1349d675d9881805df21e1c9b58d1f9b
vagrant <tk95@live.ca>
2019-07-31 23:05:41 +0000

    Script in profile.js is loading the maps from the specific user into the containers

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,6 +1,7 @@
-// // Function runs on profile page load - driver code
-// $(() => {
-//     // Call gets maps for a user function to show
-//     ajaxGetAllMaps("#browse-map-wrapper");
-//   });
+// Function runs on profile page load - driver code
+$(() => {
+    // Call gets maps for a user function to show
+    ajaxGetMapsByUser("#maps-created-container", "#maps-contributed-container");
+
+  });
   
