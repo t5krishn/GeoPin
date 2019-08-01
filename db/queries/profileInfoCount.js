486492351f05dd7aeb094b26bcc57e5117b34972
vagrant <tk95@live.ca>
2019-08-01 14:48:12 +0000

    Fixed query which returned duplicate maps for contributed_maps

--- db/queries/profileInfoCount.js
+++ db/queries/profileInfoCount.js
@@ -1,8 +1,8 @@
 const profileInfoCount = function (pool, user_id) {
 
   const query = `
-      Select (SELECT count(*) FROM maps WHERE owner_id = $1)
-      , (SELECT count(maps.*) FROM pins JOIN maps ON pins.map_id = maps.id WHERE pins.user_id = $1) AS mapsContributed
+      Select (SELECT count(*) FROM maps WHERE owner_id = $1) AS maps_created
+      , (SELECT count( DISTINCT maps.id) FROM pins JOIN maps ON pins.map_id = maps.id WHERE pins.user_id = $1) AS maps_contributed
       , (SELECT count(*) FROM liked_maps WHERE user_id = $1) AS liked_maps
       , (SELECT count(liked_maps.*) FROM liked_maps JOIN maps ON maps.id = liked_maps.map_id WHERE maps.owner_id = $1) AS liked_by
   ;`;
@@ -10,7 +10,7 @@ const profileInfoCount = function (pool, user_id) {
   return pool.query(query, [user_id])
       .then(res => {
           if (res.rows) {
-              return res.rows;
+              return res.rows[0];
           } else {
               return null;
           }
