83f9df890309094459813de2b18eb723e63ee808
vagrant <tk95@live.ca>
2019-08-01 14:51:03 +0000

    Removed console.log to increase readability of logs

--- public/scripts/components/all_maps.js
+++ public/scripts/components/all_maps.js
@@ -38,7 +38,6 @@ const doesUserLike = (likedByUSER) => {
 // When done, append the entire HTML of all Maps to given element
 const addMapsToContainer = (maps, elementID) => {
   let allMapsHTML = ``;
-  console.log(maps, elementID);
   // clearContainer(elementID);
   for (const map of maps) {
     allMapsHTML += createMapHTML(map);
