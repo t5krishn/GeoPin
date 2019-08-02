8ccb3a9419c561eef19b76486f9f744a3bc86a7d
Kendall Rowe <kendall.rowe312gmail.com>
2019-08-02 03:05:17 +0000

    added collapsible buttons for maps edit

--- public/scripts/maps_edit.js
+++ public/scripts/maps_edit.js
@@ -1,5 +1,41 @@
 // Function runs on edit page load - driver code
 $(() => {
+
+  const toggleForContainer = ($element, idString, direction, collapseString, $container, offScreenValue) => {
+    if ($($container).attr("id") === idString) {
+      $element.css(direction, 0);
+    } else {
+      $element.css(direction, offScreenValue);
+      $(collapseString).css(direction, 0);
+    }
+  };
+
+  $(".side-bar-collapse").on('click', (event) => {
+    const $container = $(event.target.closest(".edit-container"));
+    const $aboutMapContainer = $("#about-map-container-cl");
+    const $pinContainer = $("#pin-container-cl");
+    const $searchContainer = $("#search-container-cl");
+    const $searchResultsContainer = $("#search-results-container-cl");
+
+    toggleForContainer($aboutMapContainer, "about-map-container-cl", "right", "#about-map-container", $container, "-800px");
+    toggleForContainer($pinContainer, "pin-container-cl", "right", "#pin-container", $container, "-800px");
+    toggleForContainer($searchContainer, "search-container-cl", "left", "#search-container", $container, "-800px");
+    toggleForContainer($searchResultsContainer, "search-results-container-cl", "left", "#search-results-container", $container, "-800px");
+  });
+
+  $(".edit-container").on('click', (event) => {
+    const $container = $(event.target.closest(".edit-container"));
+    const $aboutMapContainer = $("#about-map-container");
+    const $pinContainer = $("#pin-container");
+    const $searchContainer = $("#search-container");
+    const $searchResultsContainer = $("#search-results-container");
+
+    toggleForContainer($aboutMapContainer, "about-map-container", "right", "#about-map-container-cl", $container, "-800px");
+    toggleForContainer($pinContainer, "pin-container", "right", "#pin-container-cl", $container, "-800px");
+    toggleForContainer($searchContainer, "search-container", "left", "#search-container-cl", $container, "-800px");
+    toggleForContainer($searchResultsContainer, "search-results-container", "left", "#search-results-container-cl", $container, "-800px");
+  });
+
   const focusOnCreatedPin = (pin, openInfoWindowNow) => {
     closeInfoWindowIfPresent();
 
