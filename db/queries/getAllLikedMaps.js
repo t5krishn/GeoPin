c45ddc3c632b2ebe8a264e24d1b2c41835adab02
Jessie Wang <wangj.5415@gmail.com>
2019-08-02 01:04:21 +0000

    fixed profile page queries to return username

--- db/queries/getAllLikedMaps.js
+++ db/queries/getAllLikedMaps.js
@@ -1,9 +1,10 @@
 const getAllLikedMaps = function (pool, user_id) {
 
   const query = `
-    SELECT maps.*
+    SELECT maps.*, users.username AS username
     FROM liked_maps
     JOIN maps ON maps.id = liked_maps.map_id
+    JOIN users ON maps.owner_id = users.id
     WHERE liked_maps.user_id = $1
     ;`;
   return pool.query(query, [user_id])
