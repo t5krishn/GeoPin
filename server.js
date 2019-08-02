7a663af5429a7e3819503281503fdd44c018eebf
vagrant <tk95@live.ca>
2019-08-02 14:45:13 +0000

    Included method override

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app        = express();
 const morgan     = require("morgan");
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcrypt");
+const methodOverride = require("method-override");
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -23,6 +24,7 @@ pool.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
